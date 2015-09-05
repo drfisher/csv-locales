70854e1b3af7f3cda9d267ada3ce96302e45b731
Pavel Rybin <drfisherozersk@gmail.com>
2015-09-05 14:52:21 +0300

    Finished base structure. Added the first test.

--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,64 @@
+var chai = require('chai');
+var csvLocales = require('../lib');
+var fs = require('fs-extra');
 var path = require('path');
-var createLocales = require('../lib');
 
-var params = {
-  csvPath: __dirname + path.sep + '../locales.csv'
+var testParams = {
+  csvPath: 'locales.csv',
+  dist: '../tmp/_locales',
+  debug: true
 };
 
-createLocales(params, function (err, json) {
-  if (err) {
-    throw err;
-  }
-  console.log('Type of json is %s', typeof json);
-  console.log(json);
+/**
+ * Creates locales
+ * @param {object} params  Locales creation params
+ * @returns {Promise}
+ */
+function createLocales (params) {
+  return new Promise(function (resolve, reject) {
+    csvLocales(params, function (err) {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+  });
+}
+
+/**
+ * Clear locales
+ * @returns {Promise}
+ */
+function clearLocales () {
+  return new Promise(function (resolve, reject) {
+    try {
+      fs.removeSync(testParams.dist);
+      resolve();
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+/*
+ * Tests
+ */
+describe('csv-locales', function () {
+
+  // Make absolute paths in testParams
+  before(function () {
+    var dir = __dirname + path.sep;
+    ['csvPath', 'dist'].forEach(function (curPath) {
+      testParams[curPath] = path.normalize(dir + testParams[curPath]);
+    });
+  });
+
+  it('should work without errors', function () {
+    return createLocales(testParams);
+  });
+
+  // Clear after tests
+  after(function () {
+    clearLocales();
+  });
 });
