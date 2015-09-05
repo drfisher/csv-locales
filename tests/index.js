a2323c91f0b4e5e81a11e1b287ad460d7ea7404d
Pavel Rybin <drfisherozersk@gmail.com>
2015-09-05 16:57:37 +0300

    Added some tests

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,50 @@
-var chai = require('chai');
+var assert = require('chai').assert;
 var csvLocales = require('../lib');
 var fs = require('fs-extra');
 var path = require('path');
+var iterator = require('object-recursive-iterator');
+var getByPath = require('getbypath');
 
-var testParams = {
+var localeParams = {
   csvPath: 'locales.csv',
   dist: '../tmp/_locales',
-  debug: true
+  debug: false
+};
+
+var testParams = {
+  locales: ['ru', 'en'],
+  // jscs:disable
+  examples: {
+    en: {
+      ext_name: {
+        message: 'My localized extension',
+        description: 'name of the extension'
+      },
+      ext_discription: {
+        message: 'This is an example of extension\'s short description',
+        description: 'description of the extension'
+      },
+      options_title: {
+        message: 'Options',
+        description: 'The title of a settings page (for the title tag)'
+      }
+    },
+    ru: {
+      ext_name: {
+        message: 'Мое локализованное расширение',
+        description: 'Название расширения'
+      },
+      ext_discription: {
+        message: 'Это пример краткого описания расширения',
+        description: 'Описание расширения'
+      },
+      options_title: {
+        message: 'Страницы настроек',
+        description: 'Заголовок страницы с настройками (для тега title)'
+      }
+    }
+  }
+  // jscs:enable
 };
 
 /**
@@ -32,7 +70,7 @@ function createLocales (params) {
 function clearLocales () {
   return new Promise(function (resolve, reject) {
     try {
-      fs.removeSync(testParams.dist);
+      fs.removeSync(localeParams.dist);
       resolve();
     } catch (err) {
       reject(err);
@@ -44,17 +82,54 @@ function clearLocales () {
  * Tests
  */
 describe('csv-locales', function () {
+  var messages = {};
+  var localesTotal = testParams.locales.length;
 
   // Make absolute paths in testParams
   before(function () {
     var dir = __dirname + path.sep;
     ['csvPath', 'dist'].forEach(function (curPath) {
-      testParams[curPath] = path.normalize(dir + testParams[curPath]);
+      localeParams[curPath] = path.normalize(dir + localeParams[curPath]);
     });
   });
 
-  it('should work without errors', function () {
-    return createLocales(testParams);
+  it('should works without errors', function () {
+    return createLocales(localeParams);
+  });
+
+  it('should creates ' + localesTotal + ' locales',
+    function () {
+      var count = 0;
+      testParams.locales.every(function (localeName) {
+        try {
+          var jsonPath = path.normalize([
+            localeParams.dist, localeName, 'messages.json'
+          ].join(path.sep));
+          var json = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+
+          assert.typeOf(json, 'object');
+          messages[localeName] = json;
+          count++;
+          return true;
+
+        } catch (err) {
+          return false;
+        }
+      });
+      assert.equal(count, localesTotal);
+    });
+
+  it('should creates json with a valid structure', function () {
+    testParams.locales.forEach(function (localeName) {
+      iterator.forAll(messages[localeName], function (path, key, obj) {
+        if (typeof obj[key] === 'string') {
+          assert.equal(
+            obj[key],
+            getByPath(testParams.examples[localeName], path + '.' + key)
+          );
+        }
+      });
+    });
   });
 
   // Clear after tests
